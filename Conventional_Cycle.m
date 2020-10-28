e65223f7d4e4904bedc2927db6b50458d372b7b5
dariff23 <73076800+dariff23@users.noreply.github.com>
2020-10-28 17:30:16 -0400

    Updates for Conventional System Code
    Added code for finding mass flow rate, heat transfer of heat pump for cooling and heating modes. Also added code to the Values_for_Conventional_Cycle script to find high and low P and to plot T-s and P-h diagrams for Conventional Cycle

--- Conventional_Cycle.m
+++ Conventional_Cycle.m
@@ -1,4 +1,4 @@
-function [T,s,P,h,COP] = Conventional_Cycle(P1,P3,substance)
+function [T,s,P,h,massFlowrateSubstance] = Conventional_Cycle(P1,P3,QNeeded,substance,file)
 
 load(file, 'tempC', 'timestamp')
 addpath('..\Project-1b\ThermoTablesCoolProp_v6_1_0')
@@ -10,9 +10,10 @@ T1 = CoolProp.PropsSI('T','P',P1,'Q',Q1,substance);
 s1 = CoolProp.PropsSI('S','P',P1,'Q',Q1,substance);
 h1 = CoolProp.PropsSI('H','P',P1,'Q',Q1,substance);
 % State 2a
+P2 = P3;
 s2a = s1;
-T2a = CoolProp.PropsSI('T','P',P3,'S',s2a,substance);
-h2a = CoolProp.PropsSI('H','P',P3,'S',s2a,substance);
+T2a = CoolProp.PropsSI('T','P',P3,'S',s1,substance);
+h2a = CoolProp.PropsSI('H','P',P3,'S',s1,substance);
 % State 2b
 T2b = CoolProp.PropsSI('T','P',P3,'Q',Q1,substance);
 s2b = CoolProp.PropsSI('S','P',P3,'Q',Q1,substance);
@@ -28,7 +29,7 @@ P4 = P1;
 s4 = CoolProp.PropsSI('S','P',P1,'H',h4,substance);
 
 % Process 1-2
-P1_2 = linspace(P1,P3,100);
+P1_2 = linspace(P1,P2,100);
 for i = 1:length(P1_2)
     h1_2(i) = CoolProp.PropsSI('H','P',P1_2(i),'S',s2a,substance);
 end
@@ -38,20 +39,27 @@ for i = 1:length(s3_4)
     T3_4(i) = CoolProp.PropsSI('T','S',s3_4(i),'H',h4,substance);
 end
 % Process 2a-2b
-T2a_2b = linspace(T2a,T2b,100);
-for i = 1:length(T2a_2b)
-    s2a_2b(i) = CoolProp.PropsSI('S','T',T2a_2b(i),'P',P3,substance);
+% T2a_2b = linspace(T2a,T2b,100);
+% for i = 1:length(T2a_2b)
+%     s2a_2b(i) = CoolProp.PropsSI('S','T',T2a_2b(i),'P',P3,substance);
+% end
+
+T = [T1,T2a,T2b,T3_4,T1]-273.15;
+s = [s1,s2a,s2b,s3_4,s1]./(10^3);
+P = [P1_2,P3,P4,P1]./(10^3);
+h = [h1_2,h3,h4,h1]./(10^3);
+
+QCooling = min(QNeeded);
+disp(QCooling);
+QHeating = max(QNeeded);
+disp(QHeating);
+massFlowrateCooling = (h1-h4)/QCooling;
+massFlowrateHeating = (h2a-h3)/QHeating;
+if massFlowrateHeating > massFlowrateCooling
+    massFlowrateSubstance = massFlowrateHeating;
+elseif massFlowrateHeating < massFlowrateCooling
+    massFlowrateSubstance = massFlowrateCooling;
 end
-
-T = [T1,T2a_2b,T3_4,T1];
-s = [s1,s2a_2b,s3_4,s1];
-P = [P1_2,P3,P4,P1];
-h = [h1_2,h3,h4,h1];
-TL = T1;
-TH = T2a;
-
-% Will need code added to calculate the COP for heating and cooling modes
-
 
 end
 
--- Values_for_Conventional_Cycle.m
+++ Values_for_Conventional_Cycle.m
@@ -1,15 +1,62 @@
+clear all
+close all
+
 % Code for finding PL and PH using TL and TH
+file = 'weather_2019.mat';
 load(file, 'tempC', 'timestamp')
 addpath('..\Project-1b\ThermoTablesCoolProp_v6_1_0')
 
-TL = min(tempC);
-for i = 1:length(tempC)
-    if mode = cooling
-        TH = [20,21,22,23];
-    elseif mode = heating
-        TH = [17,18,19,20];
-    end
+substance = 'Air';
+wallArea = 81.59471;
+wallResistance = 5.205;
+windowArea = 30.28696;
+windowResistance = 0.1905;
+massFlowrate = 0.248;
+Patm = 101325;
+QHuman = 100;
+[QConduction, QVentilation, QPeople, QSum, QNeeded, time] = HeatCoolLoads(massFlowrate,wallResistance,windowResistance,QHuman,file);
+% called 'HeatCoolLoads' function to find relevent Q terms
+substance = 'R410a';
+TLow = min(tempC);
+disp(TLow);
+THigh = max(tempC);
+disp(THigh);
+PLow = CoolProp.PropsSI('P','T',TLow+273.15,'Q',1,substance);
+%sLow = CoolProp.PropsSI('S','T',TLow,'Q',1.0,substance);
+PHigh = CoolProp.PropsSI('P','T',THigh+273.15,'Q',0,substance);
+[T_C,s_C,P_C,h_C,massFlowrate2] = Conventional_Cycle(PLow,PHigh,QNeeded,substance,file);
+% called 'Coventional_Cycle' function to find mdot for R-410a and power for
+% heat pump
+
+% P-v vapor dome
+steps = 100;
+P = linspace(50,4860,steps*5)*10^3;
+for i=1:length(P)
+    %Getting vapour dome enthalpy and converting to kJ/kg
+    hliq(i) = CoolProp.PropsSI('H','P',P(i),'Q',0,'R410a')./1000;
+    hvap(i) = CoolProp.PropsSI('H','P',P(i),'Q',1,'R410a')./1000;
 end
-PL = 200000;
-PH = 1000000;
-% massflowrate = 0.248;
+%converting the vapour dome pressure to kPa
+P = P./1000;
+
+% T-s vapor dome
+T = linspace(-50,71.3,steps*3)+273.15;
+for i=1:length(T)
+    %Getting vapour dome entropy and converting to kJ/kg-k
+    sliq(i) = CoolProp.PropsSI('S','T',T(i),'Q',0,'R410a')./1000;
+    svap(i) = CoolProp.PropsSI('S','T',T(i),'Q',1,'R410a')./1000;
+end
+%converting the vapour dome temperature to celcius
+T = T-273.15;
+
+figure
+hold on
+plot([sliq,flip(svap)],[T,flip(T)],'Color', '#00ADEF', 'LineWidth', 2)
+plot(s_C,T_C)
+title('T-s Diagram')
+
+figure
+hold on
+plot([hliq,flip(hvap)],[P,flip(P)], 'Color', '#00ADEF', 'LineWidth', 2)
+plot(h_C,P_C)
+title('P-h Diagram')
