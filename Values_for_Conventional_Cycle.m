950c8136fb95ef3962858d4dd1f45aa1236a8f76
dariff23 <73076800+dariff23@users.noreply.github.com>
2020-10-28 12:42:36 -0400

    New Conventional Cycle scripts
    Coppied my compression cycle function from Part1A, and created a new script to establish values for PH, PL, as well as to establish varying TH values for both heating and cooling modes according the the instructions

---
+++ Values_for_Conventional_Cycle.m
@@ -0,0 +1,14 @@
+% Code for finding PL and PH using TL and TH
+load(file, 'tempC', 'timestamp')
+addpath('..\Project-1b\ThermoTablesCoolProp_v6_1_0')
+
+TL = min(tempC);
+for i = 1:length(tempC)
+    if mode = cooling
+        TH = [20,21,22,23];
+    elseif mode = heating
+        TH = [17,18,19,20];
+    end
+end
+PL = 200000;
+PH = 1000000;
