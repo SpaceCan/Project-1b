16dc529c2c92e76806061ad895ed44c4b677cbb1
dariff23 <73076800+dariff23@users.noreply.github.com>
2020-11-02 12:23:14 -0500

    Update Newer_Cycle.m
    added rest of commands to pull T,s,P,h values.

--- Newer_Cycle.m
+++ Newer_Cycle.m
@@ -1,8 +1,8 @@
 function [T,s,P,h,massFlowrate,] = Newer_Cycle(tempOutside,tempInside,deltaT,substance)
 %Finds COP for the vapour compression cycle given matrices T_H and T_L
 
-Q1 = 0;
-Q2 = 1;
+Q1 = 1;
+Q2 = 0;
 %Calculating T1 and T3
 if tempOutside < tempInside
     T1 = tempOutside - deltaT + 273.15;
@@ -16,16 +16,25 @@ end
 %cop = zeros(size(T1,1),size(T1,2));
 
 % State 1
-s1 = CoolProp.PropsSI('S', 'T', T1, 'Q', Q2, substance);
-h1 = CoolProp.PropsSI('H', 'T', T1, 'Q', Q2, substance);
-% State 2   
+s1 = CoolProp.PropsSI('S', 'T', T1, 'Q', Q1, substance);
+P1 = CoolProp.PropsSI('P', 'T', T1,'S', s1, substance);
+h1 = CoolProp.PropsSI('H', 'T', T1, 'Q', Q1, substance);
+% State 2  
+s2a = s1;
 P2 = CoolProp.PropsSI('P', 'T', T3, 'Q', Q1, substance);
-T2 = CoolProp.PropsSI('T', 'S', s1, 'P', P2, substance);
-h2 = CoolProp.PropsSI('H', 'T', T2, 'S', s1, substance);
+T2 = CoolProp.PropsSI('T', 'S', s2a, 'P', P2, substance);
+h2 = CoolProp.PropsSI('H', 'T', T2, 'S', s2a, substance);
+s2b = CoolProp.PropsSI('S', 'P', P2, 'Q', Q1, substance);
+T2b = CoolProp.PropsSI('T', 'P', P2, 'Q', Q1, substance);
 % State 3
-h3 = CoolProp.PropsSI('H', 'T', T3, 'Q', Q1, substance);
-% State 4   
+s3 = CoolProp.PropsSI('S', 'T', T3, 'Q', Q2, substance);
+h3 = CoolProp.PropsSI('H', 'T', T3, 'Q', Q2, substance);
+P3 = CoolProp.PropsSI('P', 'T', T3, 'Q', Q2, substance);
+% State 4
 h4 = h3;
+P4 = P1;
+T4 = CoolProp.PropsSI('T', 'P', P4, 'H', h4, substance);
+s4 = CoolProp.PropsSI('S', 'P', P4, 'H', h4, substance);
 
 q_h = (h2 - h3);
 q_l = (h1 - h4);
